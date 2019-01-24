a13b35ea014908f2ead2a46352f0182caaded525
ryan m <contact@ryanmeek.com>
2019-01-24 16:26:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,8 +23,12 @@ def position_taken?(board, input)
   end
 end
 
-def valid_move?
-  
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index < 8
+    return true
+  else
+    return false
+  end
 end
 
 def turn(board)
